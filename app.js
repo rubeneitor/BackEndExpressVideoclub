4cb8ef086b8a315aeddd2b54f2600baf69d951cb
rubeneitor <56588038+rubeneitor@users.noreply.github.com>
2019-11-15 17:16:11 +0100

    AgreadoMostrarPeliPorTituloYCorreciones
    He añadido el endpoint donde muestra las peliculas por el titulo que se le pasa por parametro y arreglado algunos errores en el endpoin de logging de usuario

--- app.js
+++ app.js
@@ -56,11 +56,11 @@ app.post('/user/register', async (req,res) => {
 })
 
 //Logueamos por username
-// app.get('/user/:username', (req, res)=>{
-//     UserModel.findOne(username:pedro3)
-//     .then(user=>res.send(user))
-//     .catch(error=>console.log(error))
-// })
+app.get('/user/:username', (req, res)=>{
+    UserModel.findOne({username:req.params.username})
+    .then(user=>res.send(user))
+    .catch(error=>console.log(error))
+})
 
 //Actualiza un usuario existente
 // app.patch('/user/:id', (req, res)=>{
@@ -79,17 +79,25 @@ app.post('/user/register', async (req,res) => {
 // })
 
 //Muestra todas las peliculas
-app.get('/pelicula', (req, res)=>{
+app.get('/pelicula', (req, res, next)=>{
     PeliculaModel.find({})
     .then(movies=>res.send(movies))
     .catch(error=>console.log(error))
+   
 })
 
 //Musetra la pelcula por id
-app.get('./pelicula/:id', (req, res) => {
-    PeliculaModel.findById(req.params.id)
+app.get('/pelicula/:id', (req, res) => {
+    PeliculaModel.findOne({id:parseInt(req.params.id)})
     .then(movie => res.send(movie))
     .catch(error=>console.log(error))
+})
+
+//Muestra la pelicula por titulo
+app.get('/pelicula/:title', (req, res) => {
+    PeliculaModel.findOne({title:req.params.title})
+    .then(movie => res.send(movie))
+    .catch(error => console.log(error));
 })
 
 
