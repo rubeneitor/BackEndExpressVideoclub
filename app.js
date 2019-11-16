bed00ef9086e8ae548e43dd9d08108138a806e23
rubeneitor <56588038+rubeneitor@users.noreply.github.com>
2019-11-16 20:56:27 +0100

    AgragadoToken

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+
+var jwt = require('jsonwebtoken')
+var bodyParser = require('body-parser')
+
 const UserModel = require('./models/User');
 const PeliculaModel = require('./models/Pelicula');
 const mongoose=require('mongoose')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json({limit:'10mb'}))
 
 app.use(express.json());
 
@@ -115,6 +122,57 @@ app.get('/pelicula/:title', (req, res) => {
 //       .status(400)
 //       .json({ message: `password too short` });
 //  }
+
+
+
+app.post('/login', (req, res) => {
+    var username = req.body.user
+    var password = req.body.password
+  
+    if( !(username === UserModel.username && password === UserModel.password)){
+      res.status(401).send({
+        error: 'usuario o contraseña inválidos'
+      })
+      return
+    }
+  
+    var tokenData = {
+      username: username
+      // ANY DATA
+    }
+  
+    var token = jwt.sign(tokenData, 'Secret Password', {
+       expiresIn: 60 * 60 * 24 // expires in 24 hours
+    })
+  
+    res.send({
+      token
+    })
+  })
+
+  app.get('/secure', (req, res) => {
+    var token = req.headers['authorization']
+    if(!token){
+        res.status(401).send({
+          error: "Es necesario el token de autenticación"
+        })
+        return
+    }
+
+    token = token.replace('Bearer ', '')
+
+    jwt.verify(token, 'Secret Password', function(err, user) {
+      if (err) {
+        res.status(401).send({
+          error: 'Token inválido'
+        })
+      } else {
+        res.send({
+          message: 'Awwwww yeah!!!!'
+        })
+      }
+    })
+})
 
 
 
